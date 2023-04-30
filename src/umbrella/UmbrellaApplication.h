fc8cd18c9a2b0af45c5f9b3ae6d680d92d93075a
Davi <davicr38@gmail.com>
2023-04-29 21:01:55 -0300

    Add [[nodiscard]] to Result enumerations

--- src/umbrella/UmbrellaApplication.h
+++ src/umbrella/UmbrellaApplication.h
@@ -7,14 +7,14 @@ struct GLFWwindow;
 
 namespace Umbrella {
 
-enum class InitializeResult : uint8_t {
+enum class [[nodiscard]] InitializeResult : uint8_t {
     InitializeOk = 0,
     GLFWInitFail = 1,
     WindowCreationFail = 2,
     GLADLoaderFail = 3
 };
 
-enum class PrepareResult : uint8_t {
+enum class [[nodiscard]] PrepareResult : uint8_t {
     PrepareOk = 0,
     SourceReadFail = 1,
     ShaderBuildFail = 2
